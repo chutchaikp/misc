479d3f9543becbfc91e4da773b4b3f6086c7fffc
chutchai.kp <chutchai.kp@gmail.com>
2023-02-15 07:36:12 +0700

    commit

---
+++ src/components/navbar/Other.jsx
@@ -0,0 +1,9 @@
+import './other.scss';
+const Other = () => {
+  return (
+    <div className="other">
+      <h1> Other </h1>
+    </div>
+  );
+};
+export default Other;
--- src/components/navbar/Otherx.jsx
+++
@@ -1,9 +0,0 @@
-import './otherx.scss';
-const Otherx = () => {
-  return (
-    <div className="otherx">
-      <h1> Otherx </h1>
-    </div>
-  );
-};
-export default Otherx;
